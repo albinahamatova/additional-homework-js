1f9c55fb08fa4afada4481b7021efbbc6b478fbd
albinahamatova <albina.hamatova@gmail.com>
2026-09-24 22:33:09 +0300

    hw 1

---
+++ main.js
@@ -0,0 +1,135 @@
+const name = "Albina";
+const age = "36";
+const city = "Kazan";
+const isStudent = true;
+const price = 5000;
+// выведется Amina
+// выведется 15
+// выведется 7
+// const age=20; const score=0; const siteName='Shop'; let count=1
+// потому что const неизменяемый
+// const userName="Amina"
+// const name1="Amina"
+// string
+// number
+// boolean
+// string
+// boolean
+// undefined
+// true
+// false
+// true
+// false
+// true
+//false
+// true
+// false - потому что переменная задана строкой 20, и сравнивается строка с числом
+// можно
+// ничего не выведется
+// вход запрещен
+// 30 задание
+const temperature = 30;
+if (temperature > 25) {
+  console.log("Жарко");
+} else {
+  console.log("Не жарко");
+}
+// 31 задание
+const age31 = 19;
+if (age31 >= 18) {
+  console.log("Совершеннолетний");
+} else {
+  console.log("Несовершеннолетний");
+}
+// 32 задание
+const score = 80;
+if (score >= 60) {
+  console.log("Зачет");
+} else {
+  console.log("Незачет");
+}
+// 33 задание
+const password = "12345";
+if (password === "12345") {
+  console.log("Добро пожаловать");
+} else {
+  console.log("Неверный пароль");
+}
+// 34 задание
+// Пользователь онлайн
+// 35 задание
+// Билета нет
+// 36 задание
+const age36 = 15;
+if (age36 >= 18) {
+  console.log("Можно войти");
+} else {
+  console.log("Вход запрещен");
+}
+// 37 задание
+const age37 = 18;
+if (age37 >= 18) {
+  console.log("Можно");
+}
+// 38 задание
+const age38 = 18;
+if (age38 === 18) {
+  console.log("Возраст 18");
+}
+// 39 задание
+// А
+// 40 задание
+// успех
+// 41 задание
+// Дорого
+// 42 задание
+// возраст не подходит, т.к. сравнивается строка с числом
+// 43 задание
+const number = 3;
+if (number > 0) {
+  console.log("Число положительное");
+} else {
+  console.log("Число отрицательное");
+}
+// 44 задание
+const tempetature = 20;
+if (temperature >= 25) {
+  console.log("Жарко");
+} else {
+  console.log("Прохладно");
+}
+// 45 задание
+const score45 = 70;
+if (score45 >= 60) {
+  console.log("Экзамен сдан");
+} else {
+  console.log("Экзамен не сдан");
+}
+// 46 задание
+const isLoggedIn = true;
+if (isLoggedIn) {
+  console.log("Добро пожаловать");
+} else {
+  console.log("Пожалуйста, войдите в аккаунт");
+}
+// 47 задание
+const age47 = 20;
+if (age47 >= 18) {
+  console.log("Можно войти");
+} else {
+  console.log("Нельзя войти");
+}
+// 48 задание
+// создали переменную =значение/счетчик равное 2
+// изменили значение на исходное+3
+// написали программу если переменная больше или равна 5, то выведется "А", иначе выведется "В"
+// 49 задание
+// Второй. Т.к указано не равно 10
+// 50 задание
+let age50 = 17;
+age50 = 20;
+if (age50 >= 18) {
+  console.log("Можно войти");
+} else {
+  console.log("Нельзя войти");
+}
